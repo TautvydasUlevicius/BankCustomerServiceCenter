ac601b57a796bcbc5205f221396996407f222841
Tautvydas Ulevičius <t.ulevicius@paysera.com>
2018-10-30 14:24:38 +0200

    Project fully redone

---
+++ src/Entity/Operation.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Operation
+{
+    /** @var string */
+    private $operationDate;
+
+    /** @var string */
+    private $userId;
+
+    /** @var string */
+    private $userType;
+
+    /** @var string */
+    private $operationType;
+
+    /** @var float */
+    private $amount;
+
+    /** @var string */
+    private $currency;
+
+    public function setDate(string $operationDate): Operation
+    {
+        $this->operationDate = $operationDate;
+
+        return $this;
+    }
+
+    public function getDate(): string
+    {
+        return $this->operationDate;
+    }
+
+    public function setUserId(string $userId): Operation
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+
+    public function setUserType(string $userType): Operation
+    {
+        $this->userType = $userType;
+
+        return $this;
+    }
+
+    public function getUserType(): string
+    {
+        return $this->userType;
+    }
+
+    public function setOperationType(string $operationType): Operation
+    {
+        $this->operationType = $operationType;
+
+        return $this;
+    }
+
+    public function getOperationType(): string
+    {
+        return $this->operationType;
+    }
+
+    public function setAmount(float $amount): Operation
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function setCurrency(string $currency): Operation
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+}
